0d0ec9ced9fd4bb8c47dd9359694e86bf358d70d
igor <igor@igor.io>
2018-05-06 16:45:44 +0200

    more test cases for index scan

--- spec/plan/index_scan_spec.rb
+++ spec/plan/index_scan_spec.rb
@@ -8,10 +8,10 @@ RSpec.describe Kwery::Plan::IndexScan do
       id:     Kwery::Catalog::Column.new(:integer),
       name:   Kwery::Catalog::Column.new(:string),
     },
-    indexes: [:users_idx_id],
+    indexes: [:users_idx_name],
   )
-  catalog.index :users_idx_id, Kwery::Catalog::Index.new(:users, [
-    Kwery::Catalog::IndexedExpr.new(Kwery::Expr::Column.new(:id), :asc),
+  catalog.index :users_idx_name, Kwery::Catalog::Index.new(:users, [
+    Kwery::Catalog::IndexedExpr.new(Kwery::Expr::Column.new(:name), :asc),
   ])
 
   schema = catalog.new_schema
@@ -27,25 +27,25 @@ RSpec.describe Kwery::Plan::IndexScan do
     {id: 9,  name: "Uta"},
     {id: 10, name: "Anastasia"}
   ])
-  schema.reindex(:users, :users_idx_id)
+  schema.reindex(:users, :users_idx_name)
 
-  it "scans the whole index" do
-    plan = Kwery::Plan::IndexScan.new(:users, :users_idx_id)
+  it "scans the whole index in sorted order" do
+    plan = Kwery::Plan::IndexScan.new(:users, :users_idx_name)
 
     context = Kwery::Plan::Context.new(schema)
     result = plan.call(context)
 
     expect(result.to_a).to eq([
-      {id: 1,  name: "Kathleen"},
-      {id: 2,  name: "Xantha"},
-      {id: 3,  name: "Hope"},
-      {id: 4,  name: "Hedley"},
-      {id: 5,  name: "Reese"},
-      {id: 6,  name: "Emi"},
-      {id: 7,  name: "Herrod"},
-      {id: 8,  name: "Quincy"},
-      {id: 9,  name: "Uta"},
       {id: 10, name: "Anastasia"},
+      {id: 6,  name: "Emi"},
+      {id: 4,  name: "Hedley"},
+      {id: 7,  name: "Herrod"},
+      {id: 3,  name: "Hope"},
+      {id: 1,  name: "Kathleen"},
+      {id: 8,  name: "Quincy"},
+      {id: 5,  name: "Reese"},
+      {id: 9,  name: "Uta"},
+      {id: 2,  name: "Xantha"},
     ])
 
     expect(context.stats).to eq({
@@ -54,18 +54,18 @@ RSpec.describe Kwery::Plan::IndexScan do
   end
 
   it "scans only as much as needed by limit" do
-    plan = Kwery::Plan::IndexScan.new(:users, :users_idx_id)
+    plan = Kwery::Plan::IndexScan.new(:users, :users_idx_name)
     plan = Kwery::Plan::Limit.new(5, plan)
 
     context = Kwery::Plan::Context.new(schema)
     result = plan.call(context)
 
     expect(result.to_a).to eq([
-      {id: 1,  name: "Kathleen"},
-      {id: 2,  name: "Xantha"},
-      {id: 3,  name: "Hope"},
+      {id: 10, name: "Anastasia"},
+      {id: 6,  name: "Emi"},
       {id: 4,  name: "Hedley"},
-      {id: 5,  name: "Reese"},
+      {id: 7,  name: "Herrod"},
+      {id: 3,  name: "Hope"},
     ])
 
     expect(context.stats).to eq({
@@ -76,7 +76,7 @@ RSpec.describe Kwery::Plan::IndexScan do
   it "filters properly" do
     pred = lambda { |tup| tup[:id] == 8 }
 
-    plan = Kwery::Plan::IndexScan.new(:users, :users_idx_id)
+    plan = Kwery::Plan::IndexScan.new(:users, :users_idx_name)
     plan = Kwery::Plan::Filter.new(pred, plan)
 
     context = Kwery::Plan::Context.new(schema)
@@ -94,7 +94,7 @@ RSpec.describe Kwery::Plan::IndexScan do
   it "filters lazily" do
     pred = lambda { |tup| tup[:id] == 8 }
 
-    plan = Kwery::Plan::IndexScan.new(:users, :users_idx_id)
+    plan = Kwery::Plan::IndexScan.new(:users, :users_idx_name)
     plan = Kwery::Plan::Filter.new(pred, plan)
     plan = Kwery::Plan::Limit.new(1, plan)
 
@@ -106,14 +106,14 @@ RSpec.describe Kwery::Plan::IndexScan do
     ])
 
     expect(context.stats).to eq({
-      index_tuples_scanned: 8,
+      index_tuples_scanned: 7,
     })
   end
 
   it "sorts the result set" do
     comp = lambda { |tup_a, tup_b| tup_a[:name] <=> tup_b[:name] }
 
-    plan = Kwery::Plan::IndexScan.new(:users, :users_idx_id)
+    plan = Kwery::Plan::IndexScan.new(:users, :users_idx_name)
     plan = Kwery::Plan::Sort.new(comp, plan)
 
     context = Kwery::Plan::Context.new(schema)
@@ -129,7 +129,7 @@ RSpec.describe Kwery::Plan::IndexScan do
       {id: 8,  name: "Quincy"},
       {id: 5,  name: "Reese"},
       {id: 9,  name: "Uta"},
-      {id: 2,  name: "Xantha"}
+      {id: 2,  name: "Xantha"},
     ])
 
     expect(context.stats).to eq({
@@ -140,7 +140,7 @@ RSpec.describe Kwery::Plan::IndexScan do
   it "scans everything while sorting despite limit" do
     comp = lambda { |tup_a, tup_b| tup_a[:name] <=> tup_b[:name] }
 
-    plan = Kwery::Plan::IndexScan.new(:users, :users_idx_id)
+    plan = Kwery::Plan::IndexScan.new(:users, :users_idx_name)
     plan = Kwery::Plan::Sort.new(comp, plan)
     plan = Kwery::Plan::Limit.new(2, plan)
 
@@ -160,7 +160,7 @@ RSpec.describe Kwery::Plan::IndexScan do
   it "projects selected values" do
     proj = lambda { |tup| tup[:name] }
 
-    plan = Kwery::Plan::IndexScan.new(:users, :users_idx_id)
+    plan = Kwery::Plan::IndexScan.new(:users, :users_idx_name)
     plan = Kwery::Plan::Project.new(proj, plan)
     plan = Kwery::Plan::Limit.new(2, plan)
 
@@ -168,8 +168,8 @@ RSpec.describe Kwery::Plan::IndexScan do
     result = plan.call(context)
 
     expect(result.to_a).to eq([
-      "Kathleen",
-      "Xantha",
+      "Anastasia",
+      "Emi",
     ])
 
     expect(context.stats).to eq({
